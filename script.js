3e6aa0ac2e081cb16a190cd1a48fba382198250d
SM10 <22535138+SM10@users.noreply.github.com>
2023-01-26 20:11:46 -0500

    added button to change square density

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let rowCount = 108
-let colCount = 108
+let rowCount = 90
+let colCount = 90
 let mousedown = false
 
 createGrid()
@@ -14,14 +14,16 @@ function createGrid(){
         newRow.style.justifyContent = "center"
         newRow.style.alignItems = "stretch"
         newRow.style.flex = "auto"
-        newRow.style.flexDirection = "row"
-        newRow.className = "row"
-        newRow.id = "row-" + row
+        newRow.style.flexDirection = "column"
+        newRow.style.borderRa
+        newRow.className = "column"
+        newRow.id = "column-" + row
         for(let column = 0; column < colCount; column++ )
         {
             let newCol = document.createElement("div")
             newCol.style.flex = "auto"
-            newCol.className = "column"
+            newCol.style.backgroundColor = "lightgray"
+            newCol.className = "row"
             newCol.id = "cell-" + row + "-" + column 
             newCol.addEventListener('mousedown', function(event){
                 mousedown = true
@@ -43,8 +45,46 @@ function createGrid(){
         }
         sketcharea.appendChild(newRow)
     }
+    let topleft = document.querySelector("#cell-0-0")
+    topleft.style.borderTopLeftRadius = "30px";
+
+    let topright = document.querySelector("#cell-" + (rowCount - 1) + "-0")
+    topright.style.borderTopRightRadius = "30px";
+
+    let bottomleft = document.querySelector("#cell-0-" + (colCount - 1))
+    bottomleft.style.borderBottomLeftRadius = "30px";
+
+    let bottomright = document.querySelector("#cell-" + (rowCount - 1) + "-" + (colCount - 1))
+    bottomright.style.borderBottomRightRadius = "30px";
+
+    sketcharea.style.borderRadius = "30px"
 }
 
 function sketch(event){
     event.target.style.backgroundColor = "black"
+}
+
+function resizeGrid(){
+    let sketcharea = document.querySelector("#sketcharea")
+    let sketchChildren = document.querySelectorAll('#sketcharea > *')
+    for (child of sketchChildren){
+        sketcharea.removeChild(child)
+    }
+    createGrid()
+}
+
+function changeGridSize(){
+    let squareCount = prompt("Enter number of squares per side")
+    if(!isNaN(squareCount)){
+        if(squareCount < 1){
+            squareCount = 1;
+        }
+        if(squareCount > 100){
+            squareCount = 100;
+        }
+        rowCount = squareCount
+        colCount = squareCount
+        
+        resizeGrid()
+    }
 }
